ac318f0eaffc3bce60b7ca5dd45de25d137ed35c
ayanada739 <ayanada739@gmail.com>
2024-07-25 02:03:53 +0300

    Solving Q 01 [ OptimizedBubbleSort ]

---
+++ Assignment/OptimizedBubbleSort.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class OptimizedBubbleSort
+    {
+        public static void Sort<T>(T[] array) where T : IComparable<T>
+        {
+            int n = array.Length;
+            bool swapped;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                swapped = false;
+
+                for (int j = 0; j < n - 1 - i; j++)
+                {
+                    if (array[j].CompareTo(array[j + 1]) > 0)
+                    {
+                        // Swap array[j] and array[j + 1]
+                        T temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+
+                        swapped = true;
+                    }
+                }
+
+                // If no elements were swapped, the array is sorted
+                if (!swapped)
+                    break;
+            }
+        }
+    }
+}
---
+++ Assignment/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+
+          
+        static void Main(string[] args)
+        {
+
+
+
+            #region The Bubble Sort algorithm has a time complexity of O(n^2) in its worst and average cases, which makes it inefficient for large datasets. How we can optimize the Bubble Sort algorithm And implement the code of this optimized bubble sort algorithm
+
+            //Ex:
+
+            //int[] array = { 5, 3, 8, 4, 2 };
+
+
+            //Console.WriteLine("Array before sorting:");
+            //foreach (var item in array)
+            //{
+            //    Console.Write(item + " ");
+            //}
+            //Console.WriteLine();
+
+            //OptimizedBubbleSort.Sort(array);
+
+            //Console.WriteLine("Array after sorting:");
+            //foreach (var item in array)
+            //{
+            //    Console.Write(item + " ");
+            //}
+            //Console.WriteLine(); 
+
+
+            #endregion
+
+
+
+        }
+    }
+}
